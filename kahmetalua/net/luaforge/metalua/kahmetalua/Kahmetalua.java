a150de64355ae2d8b6a9949bdcd4bf9ed7e8a24f
Fabien Fleutot <fabien@MacFabien.home>
2009-02-14 15:46:46 +0100

    added some primitives to support metalua

--- kahmetalua/net/luaforge/metalua/kahmetalua/Kahmetalua.java
+++ kahmetalua/net/luaforge/metalua/kahmetalua/Kahmetalua.java
@@ -1,5 +1,6 @@
 package net.luaforge.metalua.kahmetalua;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,20 +12,30 @@ import se.krka.kahlua.vm.JavaFunction;
 import se.krka.kahlua.vm.LuaCallFrame;
 import se.krka.kahlua.vm.LuaClosure;
 import se.krka.kahlua.vm.LuaException;
+import se.krka.kahlua.vm.LuaPrototype;
 import se.krka.kahlua.vm.LuaState;
 import se.krka.kahlua.vm.LuaTable;
 
 public class Kahmetalua {
 
+	private final JavaFunction osgetenv = new JavaFunction() {
+		public int call(LuaCallFrame frame, int arguments) {
+			String var = (String) frame.get(0);
+			String val = System.getenv(var);
+			frame.push(val);
+			return 1;
+		}
+	};
+	
 	private final JavaFunction readfile = new JavaFunction() {
-		@Override public int call(LuaCallFrame frame, int arguments) {
+		public int call(LuaCallFrame frame, int arguments) {
 
-			if (arguments != 1 || !(frame.get(1) instanceof String))
+			if (arguments != 1 || !(frame.get(0) instanceof String))
 				throw new LuaException("readfile() expects a string");
 
 			StringBuffer content = new StringBuffer();
 			try {
-				String fileName = (String) frame.get(1);
+				String fileName = (String) frame.get(0);
 				InputStream f = new FileInputStream (fileName);
 				byte[] readbuffer = new byte[4096];
 				int r;
@@ -43,9 +54,9 @@ public class Kahmetalua {
 	
 	private final JavaFunction writefile = new JavaFunction(){
 
-		@Override public int call(LuaCallFrame frame, int arguments) {
-			if (arguments != 2 || !(frame.get(1) instanceof String) 
-					|| !(frame.get(2) instanceof String))
+		public int call(LuaCallFrame frame, int arguments) {
+			if (arguments != 2 || !(frame.get(0) instanceof String) 
+					|| !(frame.get(1) instanceof String))
 				throw new LuaException("readfile() expects 2 strings");
 
 			try {
@@ -60,13 +71,43 @@ public class Kahmetalua {
 		}		
 	};
 	
+	private final JavaFunction stringundump = new JavaFunction(){
+
+		public int call(LuaCallFrame frame, int arguments) {
+			if (arguments < 1 || !(frame.get(0) instanceof String))
+				throw new LuaException("string.undump() expects a string");
+			String chunk = (String) frame.get(0);
+			ByteArrayInputStream stream = new ByteArrayInputStream(chunk.getBytes());
+			try {
+				LuaClosure f = LuaPrototype.loadByteCode(stream, frame.getEnvironment());
+				frame.push(f);
+				return 1;
+			} catch (IOException e) {
+				throw new LuaException ("Can't undump this string");
+			}
+		}
+	};
+	
 	private void loadPrimitives()
 	{
 		LuaTable table = new LuaTable();		
 		table.rawset("readfile",  readfile);
 		table.rawset("writefile", writefile);
+		table.rawset("osgetenv", osgetenv);
 		state.getEnvironment().rawset("kahmetalua", table);	
-		state.loadByteCodeFromResource("io");
+		state.pcallByteCodeFromResource("io");
+		
+		set("os", "getenv", osgetenv);
+		set("string", "undump", stringundump);
+	}
+
+	private void set(Object... objects) {
+		Object key   = objects[objects.length-2];
+		Object value = objects[objects.length-1];
+		LuaTable t = state.getEnvironment();
+		for (int i=0; i<objects.length-2; i++)
+			t = (LuaTable) t.rawget(objects[i]);
+		t.rawset(key, value);
 	}
 
 	private final LuaState state;	
@@ -81,8 +122,7 @@ public class Kahmetalua {
 	 * command line.
 	 */
 	public void call(String[] args) {
-		LuaClosure main = state.loadByteCodeFromResource("metalua");
-		state.call(main, args);		
+		state.callByteCodeFromResource("metalua", (Object[]) args);
 	}
 
 	public static void main(String[] args) {
